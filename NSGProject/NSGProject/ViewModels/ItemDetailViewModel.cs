1b01d02ffab8ffd0df73ba8c4f6d8bc3e990726e
Rajendra Mehra <razmehra@hotmail.com>
2019-10-23 19:22:21 +0530

    Add project files.

---
+++ NSGProject/NSGProject/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using NSGProject.Models;
+
+namespace NSGProject.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
