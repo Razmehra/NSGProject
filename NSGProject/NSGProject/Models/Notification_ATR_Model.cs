2b2353251f809f74350988256213ec398c8768e0
Rajendra Mehra <razmehra@hotmail.com>
2019-12-11 15:21:29 +0530

    Local Commit

---
+++ NSGProject/NSGProject/Models/Notification_ATR_Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSGProject.Models
+{
+    public class Notification_ATR_Model
+    {
+        public int Id { get; set; }
+        public string NCRID { get; set; }
+        public DateTime ATRDate { get; set; }
+        public string NCROn { get; set; }
+        public int Remark { get; set; }
+        public string NCRStatus { get; set; }
+
+    }
+}
--- NSGProject/NSGProject/Views/NotificationTabbedPage.xaml.cs
+++ NSGProject/NSGProject/Views/NotificationTabbedPage.xaml.cs
@@ -13,7 +13,7 @@ namespace NSGProject.Views
     public partial class NotificationTabbedPage : TabbedPage
     {
         private AssignedWorks assignedWorks { get; set; }
-        private List<Notification_eMB_Model> _eMBNotificationList = new List<Notification_eMB_Model>()
+        public List<Notification_eMB_Model> _eMBNotificationList = new List<Notification_eMB_Model>()
         {
             new Notification_eMB_Model{ id=1, Notification="Review eMB and Work Inspection for\nAtivity Name: as submitted by contractor on dated 03-10-2019 "},
             new Notification_eMB_Model{ id=1, Notification="Review eMB and Work Inspection for\nAtivity Name: as submitted by contractor on dated 04-10-2019 "},
@@ -31,6 +31,8 @@ namespace NSGProject.Views
             this.BindingContext = assignedWorks;
             _eMBNotificationCount = _eMBNotificationList.Count();
             EMBNotificationView.Title = $"eMB({_eMBNotificationCount})";
+            LV_eMBNotification.BindingContext = _eMBNotificationList;
+            LV_eMBNotification.ItemsSource = _eMBNotificationList;
         }
 
         public void InitializePage(AssignedWorks work=null)
